b25b2be66630939a87877e8b613243fb40335216
Augie Bren <Augie>
2022-06-21 19:08:33 -0500

    with Context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './DataContext'
 
 
 function App() {
-  // search
   const [search, setSearch] = useState('')
   const [message, setMessage] = useState('Search for Music!')
   const [data, setData] = useState([])
-  // message
-  // data
+  
   const handleSearch = (e, term) => {
     e.preventDefault()
     setSearch(term)
@@ -36,7 +35,9 @@ function App() {
     <div className="App">
       <SearchBar setSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
